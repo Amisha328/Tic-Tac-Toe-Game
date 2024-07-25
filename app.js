c92ee0982b6229d091ced01757952ca6114131c2
Amisha Sahu <amishasahu328@gmail.com>
2024-07-25 11:30:21 +0530

    Updated the stysling part

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let resetButton = document.querySelector("#reset-btn");
 let newButton = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".message-container");
 let msg = document.querySelector(".msg");
+let main = document.querySelector("main");
 
 let turnO = true; // playerX, playerO
 
@@ -17,55 +18,51 @@ const winPatterns = [
     [6, 7, 8],
 ];
 
-
 boxes.forEach((box) => {
-    box.addEventListener("click", () =>{
-        console.log("Box was clicked");
+    box.addEventListener("click", () => {
         if(turnO){
             box.innerText = "O";
-            turnO = false;
             box.style.color = "rgb(147, 6, 6)";
-        }
-        else{
+        } else {
             box.innerText = "X";
-            turnO = true;
             box.style.color = "green";
         }
+        turnO = !turnO;
         box.disabled = true;
         checkWinner();
     })
-})
+});
 
 const resetGame = () => {
     turnO = true;
     enableBoxes();
     msgContainer.classList.add("hide");
+    main.classList.remove("hide");
 }
 
-const disableBoxes = () =>{
-    for(let box of boxes){
-        box.disabled = true;
-    }
+const disableBoxes = () => {
+    boxes.forEach(box => box.disabled = true);
 }
 
-const enableBoxes = () =>{
-    for(let box of boxes){
+const enableBoxes = () => {
+    boxes.forEach(box => {
         box.disabled = false;
         box.innerText = "";
-    }
+    });
 }
+
 const checkWinner = () => {
-    for(let pattern of winPatterns){
+    for (let pattern of winPatterns) {
         let posVal1 = boxes[pattern[0]].innerText;
         let posVal2 = boxes[pattern[1]].innerText;
         let posVal3 = boxes[pattern[2]].innerText;
 
-        if(posVal1 !== "" && posVal2 !== "" || posVal3 != ""){
-            if(posVal1 === posVal2 && posVal2 === posVal3){
-               msg.innerText = `Congratuation!! Winner is Player ${posVal1}`;
-               msgContainer.classList.remove("hide");
-               disableBoxes();
-            }
+        if (posVal1 !== "" && posVal1 === posVal2 && posVal2 === posVal3) {
+            msg.innerText = `Congratulations! Winner is Player ${posVal1}`;
+            msgContainer.classList.remove("hide");
+            disableBoxes();
+            main.classList.add("hide");
+            return;
         }
     }
 }
